608d5af5707637954a45bc44f88f4bd280d7af98
Damian Worobiej <damian.worobiej@gmail.com>
2026-01-29 15:48:10 +0100

    Basic Redis implementation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Cache;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<WeatherDbContext>(options => options.UseInMemoryDatabase("cacheDb"));
@@ -13,19 +14,63 @@ using (var scope = app.Services.CreateScope())
 
 app.MapGet("/weatherforecast", async (WeatherDbContext context) =>
 {
-    var forecast = await context.Forecasts.ToListAsync();
+    const string redisKey = "forecasts";
+    var redisDb = RedisDbFactory.GetDatabase();
+    
+    if (redisDb.KeyExists(redisKey))
+    {
+        var redisValue = await redisDb.StringGetAsync(redisKey);
+        var returnedData = JsonSerializer.Deserialize<List<WeatherForecast>>(redisValue.ToString());
 
-    return forecast;
+        return returnedData;
+    }
+
+    var forecasts = await context.Forecasts.ToListAsync();
+
+    var cachedData = JsonSerializer.Serialize(forecasts);
+    await redisDb.StringSetAsync(redisKey, cachedData, new TimeSpan(0, 0, 10));
+
+    return forecasts;
 });
 
 app.MapGet("/forecastsahead/{days}", async (WeatherDbContext context, int days) =>
 {
-    var forecast = await context.Forecasts
+    const string redisKey = "forecastsSet";
+
+    var redisDb = RedisDbFactory.GetDatabase();
+    var isCached = true;
+    var cachedOutput = new List<WeatherForecast>();
+    foreach (var index in Enumerable.Range(0, days + 1))
+    {
+        var key = $"{redisKey}:{DateOnly.FromDateTime(DateTime.Now.AddDays(index))}";
+        if (await redisDb.KeyExistsAsync(key) is false)
+        {
+            isCached = false;
+
+            break;
+        }
+
+        var cachedValue = await redisDb.StringGetAsync(key);
+        cachedOutput.Add(JsonSerializer.Deserialize<WeatherForecast>(cachedValue.ToString()));
+    }
+
+    if (isCached)
+    {
+        return cachedOutput;
+    }
+
+    var forecasts = await context.Forecasts
     .OrderBy(x => x.Date)
     .Take(days + 1)
     .ToListAsync();
 
-    return forecast;
+    foreach (var forecast in forecasts)
+    {
+        var date = forecast.Date.ToString();
+        await redisDb.StringSetAsync($"{redisKey}:{date}", JsonSerializer.Serialize(forecast), new TimeSpan(0, 0, 10));
+    }
+
+    return forecasts;
 });
 
 app.Run();
---
+++ RedisDbFactory.cs
@@ -0,0 +1,13 @@
+﻿using StackExchange.Redis;
+
+namespace Cache;
+
+public static class RedisDbFactory
+{
+    public static IDatabase GetDatabase()
+    {
+        var redisConnection = ConnectionMultiplexer.Connect($"localhost:6379");
+        
+        return redisConnection.GetDatabase();
+    }
+}
